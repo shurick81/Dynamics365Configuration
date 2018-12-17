a44aec74801724879e24f9c1542e6f2b278a337b
SAPOZHKOV Aleksandr <aleksandr.sapozhkov@soprasteria.com>
2018-12-18 00:58:25 +0100

    repeating publishing

--- publishing/psmodulepublishing.ps1
+++ publishing/psmodulepublishing.ps1
@@ -1,3 +1,4 @@
 Install-Module PowerShellGet -MinimumVersion 1.6.8 -Force -AllowClobber
+Remove-Item "C:\Users\$env:username\Documents\WindowsPowerShell\Modules\Dynamics365Configuration" -Recurse -Force
 Copy-Item ".\src\Dynamics365Configuration" "C:\Users\$env:username\Documents\WindowsPowerShell\Modules" -Recurse -Force
 Publish-Module -Name Dynamics365Configuration -NuGetApiKey $nuGetApiKey
