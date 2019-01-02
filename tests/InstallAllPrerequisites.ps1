785bc9d09153219d21b410d5e2295121adc5f7a4
SAPOZHKOV Aleksandr <aleksandr.sapozhkov@soprasteria.com>
2019-01-02 02:59:14 +0100

    proper prerequisites testing

--- tests/InstallAllPrerequisites.ps1
+++ tests/InstallAllPrerequisites.ps1
@@ -1,10 +1,25 @@
 # Before running this test, make sure that current directory contains prerequisite files
-Install-Dynamics365Prerequisites VisualCPlusPlusRuntime;
-if ( $LASTEXITCODE -eq 0 )
-{
-    Write-Host "Test OK";
-    Exit 0;
-} else {
-    Write-Host "Test Not OK"
-    Exit 1;
+Install-Dynamics365Prerequisites;
+@(
+    'VisualCPlusPlusRuntime',
+    'VisualCPlusPlus2010Runtime',
+    'SQLNCli2012SP4',
+    'SQLSysClrTypes2016',
+    'SharedManagementObjects2016',
+    'MSODBCSQL',
+    'SQLNCli2018R2',
+    'SQLSysClrTypes2012',
+    'SharedManagementObjects2012',
+    'ReportViewer2012'
+) | % {
+    $expectedProduct = $Dynamics365Resources.$_.IdentifyingNumber;
+    $installedProduct = gwmi Win32_Product | ? { $_.IdentifyingNumber -eq "{$expectedProduct}" }
+    if ( !$installedProduct )
+    {
+        Write-Host "Matcing products not found";
+        Exit 1;
+    } else {
+        Write-Host "Product $expectedProduct found"
+    }
 }
+Exit 0;
