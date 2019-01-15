58460d1577c48523c5fc9c0c72879ce0eabba662
SAPOZHKOV Aleksandr <aleksandr.sapozhkov@soprasteria.com>
2019-01-15 23:28:52 +0100

    importing local PS module for publishing

--- publishing/psmodulepublishing.ps1
+++ publishing/psmodulepublishing.ps1
@@ -1,4 +1,5 @@
 Install-Module PowerShellGet -MinimumVersion 1.6.8 -Force -AllowClobber
 Remove-Item "C:\Users\$env:username\Documents\WindowsPowerShell\Modules\Dynamics365Configuration" -Recurse -Force
 Copy-Item ".\src\Dynamics365Configuration" "C:\Users\$env:username\Documents\WindowsPowerShell\Modules" -Recurse -Force
+Import-Module "C:\Users\$env:username\Documents\WindowsPowerShell\Modules\Dynamics365Configuration";
 Publish-Module -Name Dynamics365Configuration -NuGetApiKey $env:NuGetApiKey
