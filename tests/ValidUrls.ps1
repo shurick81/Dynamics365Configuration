416040432820b8a34db9198d84da122f5f7638e6
SAPOZHKOV Aleksandr <aleksandr.sapozhkov@soprasteria.com>
2019-12-14 09:10:19 +0100

    calculating total file size

--- tests/ValidUrls.ps1
+++ tests/ValidUrls.ps1
@@ -1,3 +1,4 @@
+$totalSize = 0;
 $Dynamics365Resources | Get-Member -MemberType NoteProperty | % {
     $resourceName = $_.Name;
     $resourceUrl = $Dynamics365Resources.( $_.Name ).URL;
@@ -8,22 +9,23 @@ $Dynamics365Resources | Get-Member -MemberType NoteProperty | % {
         $response = Invoke-WebRequest $resourceUrl -UseBasicParsing -Method Head;
         if ( $response ) {
             if ( $response.StatusCode -eq "200" ) {
-                Write-Host "Response OK"
+                Write-Host "Response OK";
+                $totalSize = $totalSize + $response.Headers.'Content-Length';
             } else {
-                Write-Host "Response is not OK"
+                Write-Host "Response is not OK";
                 Start-Sleep 1;
             }
         } else {
-            Write-Host "Request is not OK"
+            Write-Host "Request is not OK";
             Start-Sleep 1;
         }
         $attemptsLeft--;
     } until ( ( $response.StatusCode -eq "200" ) -or ( $attemptsLeft -eq 0 ) )
     if ( $response.StatusCode -ne "200" ) {
-        Write-Host "Test is not OK"
+        Write-Host "Test is not OK";
         Exit 1;
     }
 }
-Write-Host "Test OK"
+Write-Host "Test OK";
+Write-Host "Total file size is $totalSize";
 Exit 0;
-
