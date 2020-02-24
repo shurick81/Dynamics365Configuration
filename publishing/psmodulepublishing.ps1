efdcee880ecc065f991939bdfadf5bf71b0294bb
Aleks <alexander@sapozhkov.net>
2020-02-24 21:05:11 +0100

    downloading nuget

--- publishing/psmodulepublishing.ps1
+++ publishing/psmodulepublishing.ps1
@@ -1,2 +1,3 @@
 Get-PackageProvider NuGet -ForceBootstrap
+Invoke-WebRequest -Uri https://aka.ms/psget-nugetexe -OutFile 'NuGet.exe'
 Publish-Module -Path ".\src\Dynamics365Configuration" -NuGetApiKey $env:NuGetApiKey
