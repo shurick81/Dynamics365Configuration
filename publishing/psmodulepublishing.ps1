ee09ffc6c216d044d665351c49e478087ae076e6
Aleks <alexander@sapozhkov.net>
2020-02-24 22:49:16 +0100

    importing folder in user profile

--- publishing/psmodulepublishing.ps1
+++ publishing/psmodulepublishing.ps1
@@ -1,4 +1,10 @@
 Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -RequiredVersion 2.8.5.201
-Import-Module .\src\Dynamics365Configuration\Dynamics365Configuration.psd1 -Global
+Get-ChildItem $env:USERPROFILE
+Get-ChildItem $env:USERPROFILE\Documents
+Get-ChildItem $env:USERPROFILE\Documents\WindowsPowerShell
+New-Item $env:USERPROFILE\Documents\WindowsPowerShell\Modules -ItemType Directory -ErrorAction SilentlyContinue;
+Remove-Item $env:USERPROFILE\Documents\Documents\WindowsPowerShell\Modules\Dynamics365Configuration -Recurse -Force -ErrorAction Ignore
+Copy-Item .\src\Dynamics365Configuration $env:USERPROFILE\Documents\WindowsPowerShell\Modules -Recurse -Force
+Import-Module $env:USERPROFILE\Documents\WindowsPowerShell\Modules\Dynamics365Configuration;
 Get-Module Dynamics365Configuration -ListAvailable
 #Publish-Module -Name Dynamics365Configuration -NuGetApiKey $env:NuGetApiKey
