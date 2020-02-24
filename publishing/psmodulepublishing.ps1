c31dec9d634fde9857053c7e7c0c41f0e182883e
Aleks <alexander@sapozhkov.net>
2020-02-24 22:34:28 +0100

    additional output

--- publishing/psmodulepublishing.ps1
+++ publishing/psmodulepublishing.ps1
@@ -1,3 +1,4 @@
 Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -RequiredVersion 2.8.5.201
 Import-Module .\src\Dynamics365Configuration
+Get-Module Dynamics365Configuration -ListAvailable
 Publish-Module -Name Dynamics365Configuration -NuGetApiKey $env:NuGetApiKey
