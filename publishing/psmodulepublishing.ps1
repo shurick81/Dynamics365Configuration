760b626b52b33370d53aeb13358b5670300b347d
Aleks <alexander@sapozhkov.net>
2020-02-24 21:22:48 +0100

    debug info

--- publishing/psmodulepublishing.ps1
+++ publishing/psmodulepublishing.ps1
@@ -1,2 +1,3 @@
 Install-PackageProvider -Name NuGet -Force -Scope CurrentUser -RequiredVersion 2.8.5.201
+Get-ChildItem
 Publish-Module -Path ".\src\Dynamics365Configuration" -NuGetApiKey $env:NuGetApiKey
