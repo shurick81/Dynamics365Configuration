ab9c033712026ef328791970109af249c4b97859
Aleks <alexander@sapozhkov.net>
2020-02-24 21:15:09 +0100

    installing nuget as an Azure Pipeline task

--- publishing/psmodulepublishing.ps1
+++ publishing/psmodulepublishing.ps1
@@ -1,3 +1,1 @@
-Get-PackageProvider NuGet -ForceBootstrap
-Invoke-WebRequest -Uri https://aka.ms/psget-nugetexe -OutFile 'NuGet.exe'
 Publish-Module -Path ".\src\Dynamics365Configuration" -NuGetApiKey $env:NuGetApiKey
