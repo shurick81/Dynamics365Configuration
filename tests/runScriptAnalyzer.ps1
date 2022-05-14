d8c75a057d3f4f7321cd75cccd9b72dc13417933
Aleks <aleksandr.sapozhkov@soprasteria.com>
2022-05-14 15:04:33 +0200

    Excluding PSUseSingularNouns

--- tests/runScriptAnalyzer.ps1
+++ tests/runScriptAnalyzer.ps1
@@ -1,4 +1,4 @@
-$result = Invoke-ScriptAnalyzer -Path "..\src\Dynamics365Configuration" -Recurse -ExcludeRule PSUseApprovedVerbs, PSUseDeclaredVarsMoreThanAssignments, PSAvoidUsingWMICmdlet #, PSUseSingularNouns
+$result = Invoke-ScriptAnalyzer -Path "..\src\Dynamics365Configuration" -Recurse -ExcludeRule PSUseApprovedVerbs, PSUseDeclaredVarsMoreThanAssignments, PSAvoidUsingWMICmdlet, PSUseSingularNouns
 $result
 if ( $result ) {
     throw "Version is not determined";
